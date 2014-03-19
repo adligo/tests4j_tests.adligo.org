ff9f5adac6ff938810dd3aeb4794a7c7f6d32f44
scott <scott@adligo.com>
2014-03-18 19:49:56 -0500

    ditched number asserts they can just use equals

--- src/org/adligo/jtests_tests/use_case_tests/Assert_FailConditions_Test.java
+++ src/org/adligo/jtests_tests/use_case_tests/Assert_FailConditions_Test.java
@@ -12,16 +12,12 @@ public class Assert_FailConditions_Test extends FunctionalTest {
 		assertEquals((byte) 1, (byte) 1);
 		assertEquals("hey", (byte) 1, (byte) 1);
 		
-		assertGreaterThan((byte) 1, (byte) 0);
-		assertLessThan((byte) 1, (byte) 2);
 		
 		assertNotEquals("a", "b");
 		assertNotEquals("hey","a","b");
 		assertNotEquals((byte) 1, (byte) 2);
 		assertNotEquals("hey", (byte) 1, (byte) 2);
 		
-		assertNotGreaterThan((byte) 1, (byte) 2);
-		assertNotLessThan((byte) 1, (byte) 0);
 	}
 	
 }
