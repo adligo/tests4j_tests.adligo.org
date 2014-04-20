f85125bfcb774e33973b8ba0254d2ede990cf547
scott <scott@adligo.com>
2014-04-20 02:28:03 -0500

    ok line number was fixed by simply adding visitLineNumber in the JacocoMethodInstrumenter

--- src/org/adligo/tests4j_tests/RunSingleTrial.java
+++ src/org/adligo/tests4j_tests/RunSingleTrial.java
@@ -3,14 +3,14 @@ package org.adligo.tests4j_tests;
 import org.adligo.tests4j.models.shared.system.Tests4J_Params;
 import org.adligo.tests4j.run.Tests4J;
 import org.adligo.tests4j_4jacoco.plugin.TieredJacocoPlugin;
-import org.adligo.tests4j_tests.models.shared.system.Tests4J_ParamsTrial;
+import org.adligo.tests4j_tests.api_trials.Run_ApiTrial_Trial;
 
 public class RunSingleTrial {
 
 	
 	public static void main(String [] args) {
 		Tests4J_Params params = new Tests4J_Params();
-		params.addTrial(Tests4J_ParamsTrial.class);
+		params.addTrial(Run_ApiTrial_Trial.class);
 		params.setCheckMins(false);
 		//params.setCoveragePlugin(new SimpleJacocoPlugin());
 		params.setCoveragePlugin(new TieredJacocoPlugin());
