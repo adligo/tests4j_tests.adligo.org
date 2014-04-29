89e5875623a12722727d8e5eb45f00ebcc85d894
scott <scott@adligo.com>
2014-04-29 01:15:52 -0500

    fixed logging issue in test of bad trials

---
+++ src/org/adligo/tests4j_tests/AsmClassVerifier.java
@@ -0,0 +1,50 @@
+package org.adligo.tests4j_tests;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+
+import org.adligo.tests4j.models.shared.SourceFileTrial;
+import org.adligo.tests4j_4jacoco.plugin.SimpleJacocoPlugin;
+import org.adligo.tests4j_tests.models.shared.system.Tests4J_ParamsTrial;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.Type;
+import org.objectweb.asm.util.CheckClassAdapter;
+import org.objectweb.asm.util.TraceClassVisitor;
+
+/**
+ * run this to have the ASMifier veify your ASM code if your 
+ * working on this project.
+ * 
+ * @author scott
+ *
+ */
+public class AsmClassVerifier {
+
+	public static void main(String [] args) {
+		try {
+			String internalName = "./org.adligo.tests4j_tests.models.shared.system.Tests4J_ParamsTrial.class";
+			ClassWriter cw = new ClassWriter(0);
+			
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			PrintWriter pw = new  PrintWriter(baos);
+			TraceClassVisitor tcv = new TraceClassVisitor(cw, 
+					pw);
+			CheckClassAdapter cv = new CheckClassAdapter(tcv);
+			/*
+			cv.visitMethod(Opcodes.ACC_PUBLIC, 
+					"testDefaultParams", "V", 
+					"()V", null);
+					*/
+			tcv.visit(Opcodes.V1_7,Opcodes.ACC_PUBLIC, 
+					Type.getInternalName(Tests4J_ParamsTrial.class), null, 
+					Type.getInternalName(SourceFileTrial.class), null);
+			byte [] boasA = baos.toByteArray();
+			String content = new String(boasA);
+			System.out.print(content);
+			
+		} catch (Exception x) {
+			x.printStackTrace();
+		}
+	}
+}
