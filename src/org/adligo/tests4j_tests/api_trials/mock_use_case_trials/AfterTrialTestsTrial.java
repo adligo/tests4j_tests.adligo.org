9faded18cd6247ca421ead1732a7af6df6fd8679
scott <scott@adligo.com>
2014-05-02 00:27:53 -0500

    new class

---
+++ src/org/adligo/tests4j_tests/api_trials/mock_use_case_trials/AfterTrialTestsTrial.java
@@ -0,0 +1,11 @@
+package org.adligo.tests4j_tests.api_trials.mock_use_case_trials;
+
+import org.adligo.tests4j.models.shared.AfterTrialTests;
+import org.adligo.tests4j.models.shared.UseCaseTrial;
+
+public class AfterTrialTestsTrial extends UseCaseTrial {
+
+	@AfterTrialTests
+	public  void afterTrialTests() {}
+	
+}
