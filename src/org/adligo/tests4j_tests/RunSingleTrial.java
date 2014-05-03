4242129fc0b959f2c09c60a6d8c236b7c2bc08f6
scott <scott@adligo.com>
2014-05-02 19:32:52 -0500

    some more work

--- src/org/adligo/tests4j_tests/RunSingleTrial.java
+++ src/org/adligo/tests4j_tests/RunSingleTrial.java
@@ -6,6 +6,7 @@ import org.adligo.tests4j.run.Tests4J;
 import org.adligo.tests4j.run.helpers.Tests4J_Memory;
 import org.adligo.tests4j.run.helpers.Tests4J_NotificationManager;
 import org.adligo.tests4j.run.helpers.TrialInstancesProcessor;
+import org.adligo.tests4j_4jacoco.plugin.AbstractPlugin;
 import org.adligo.tests4j_4jacoco.plugin.OldScopedJacocoPlugin;
 import org.adligo.tests4j_4jacoco.plugin.Recorder;
 import org.adligo.tests4j_tests.api_trials.Run_UseCaseTrial_ApiTrial;
@@ -17,12 +18,14 @@ public class RunSingleTrial {
 		Tests4J_Params params = new Tests4J_Params();
 		params.addTrial(Run_UseCaseTrial_ApiTrial.class);
 		//params.addTrial(Tests4J_ParamsTrial.class);
+		//params.addTrial(ByteListOutputStreamTrial.class);
 		params.setCheckMins(false);
 		ConsoleReporter reporter = new ConsoleReporter();
 		reporter.setLogOn(Recorder.class);
 		reporter.setLogOn(Tests4J_NotificationManager.class);
 		reporter.setLogOn(TrialInstancesProcessor.class);
 		reporter.setLogOn(Tests4J_Memory.class);
+		reporter.setLogOn(AbstractPlugin.class);
 		
 		params.setReporter(reporter);
 		//SimpleJacocoPlugin plugin = new SimpleJacocoPlugin();
