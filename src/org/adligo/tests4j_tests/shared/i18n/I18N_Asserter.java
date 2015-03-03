bf8d366c6eb4be73e3d5e7a9aa371381003ec902
scott <scott@adligo.com>
2015-03-02 22:52:21 -0600

    added exception when a i18n field is asserted twice.

--- src/org/adligo/tests4j_tests/shared/i18n/I18N_Asserter.java
+++ src/org/adligo/tests4j_tests/shared/i18n/I18N_Asserter.java
@@ -1,18 +1,21 @@
 package org.adligo.tests4j_tests.shared.i18n;
 
+import org.adligo.tests4j.shared.asserts.common.I_Asserts;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-
-import org.adligo.tests4j.shared.asserts.common.I_Asserts;
+import java.util.Set;
 
 public class I18N_Asserter {
 	public static final String JACOCO_DATA = "$jacocoData";
 	public static final String INSTANCE = "INSTANCE";
 	private static final List<String> IGNORED_FIELD_NAMES = getIgnoredFieldNames();
+	private Set<String> expecteds = new HashSet<String>();
 	private I_Asserts asserts;
 	private int constantCount;
 	
@@ -31,6 +34,11 @@ public class I18N_Asserter {
 	}
 	
 	public void assertConstant(String expected, String actual) {
+	  if (expecteds.contains(expected)) {
+	    throw new IllegalStateException("Already asserted " + System.lineSeparator() + 
+	        expected);
+	  }
+	  expecteds.add(expected);
 		asserts.assertEquals(expected, actual);
 		constantCount++;
 	}
@@ -42,10 +50,12 @@ public class I18N_Asserter {
 	public void assertConstantsMatchMethods(Class<?> testedClass, Collection<String> ignoredFieldNames) {
 		Field[] fields = testedClass.getDeclaredFields();
 		
+		Set<String> fieldNames = new HashSet<String>();
 		int localCount = 0;
 		for (int i = 0; i < fields.length; i++) {
 			Field field = fields[i];
 			String fieldName = field.getName();
+			fieldNames.add(fieldName);
 			if ( !ignoredFieldNames.contains(fieldName) && Modifier.isStatic(field.getModifiers())) {
 				asserts.assertTrue("field " + fieldName + " should be private",Modifier.isPrivate(field.getModifiers()));
 				asserts.assertTrue("field " + fieldName + " should be final",Modifier.isFinal(field.getModifiers()));
